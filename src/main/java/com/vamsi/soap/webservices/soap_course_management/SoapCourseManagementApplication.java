9b4f80a303621eb087a5fa4be9606861af017782
vamsi-git-test <vamsikadaru01@gmail.com>
2024-07-18 12:13:27 +0530

    first commit

---
+++ src/main/java/com/vamsi/soap/webservices/soap_course_management/SoapCourseManagementApplication.java
@@ -0,0 +1,13 @@
+package com.vamsi.soap.webservices.soap_course_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapCourseManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoapCourseManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vamsi/soap/webservices/soap_course_management/SoapCourseManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vamsi.soap.webservices.soap_course_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapCourseManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
