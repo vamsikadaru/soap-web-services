d01d1711bf964c62d06e6e512bafb20716259d3e
vamsikadaru <vamsikadaru01@gmail.com>
2024-07-20 23:46:06 +0530

    Created Endpoint to fetch all courses

--- src/main/java/com/vamsi/soap/webservices/soap_course_management/soap/CourseDetailsEndpoint.java
+++ src/main/java/com/vamsi/soap/webservices/soap_course_management/soap/CourseDetailsEndpoint.java
@@ -1,5 +1,76 @@
 package com.vamsi.soap.webservices.soap_course_management.soap;
 
-public class CourseDetailsEndpoint {
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 
+import com.vamsi.courses.CourseDetails;
+import com.vamsi.courses.GetAllCourseDetailsRequest;
+import com.vamsi.courses.GetAllCourseDetailsResponse;
+import com.vamsi.courses.GetCourseDetailsRequest;
+import com.vamsi.courses.GetCourseDetailsResponse;
+import com.vamsi.soap.webservices.soap_course_management.soap.bean.Course;
+import com.vamsi.soap.webservices.soap_course_management.soap.service.CourseDetailsService;
+
+@Endpoint
+public class CourseDetailsEndpoint {
+	
+	@Autowired
+	CourseDetailsService service;
+	
+//	GetCourseDetailsRequest
+//	GetCourseDetailsResponse
+	
+	@PayloadRoot(namespace="http://vamsi.com/courses", 
+			localPart="GetCourseDetailsRequest")
+	@ResponsePayload
+	public GetCourseDetailsResponse processCourseDetailsRequest
+		(@RequestPayload GetCourseDetailsRequest request) {
+		
+		Course course = service.findById(request.getId());
+		
+		return mapCourseDetails(course);
+	}
+
+	private GetCourseDetailsResponse mapCourseDetails(Course course) {
+		GetCourseDetailsResponse response = new GetCourseDetailsResponse();
+		
+		response.setCourseDetails(mapCourse(course));
+		
+		return response;
+	}
+	
+	private GetAllCourseDetailsResponse mapAllCourseDetails(List<Course> courses) {
+		GetAllCourseDetailsResponse response = new GetAllCourseDetailsResponse();
+		for(Course course:courses) {
+			CourseDetails mapCourse = mapCourse(course);
+			response.getCourseDetails().add(mapCourse);
+		}		
+		return response;
+	}
+
+	private CourseDetails mapCourse(Course course) {
+		CourseDetails courseDetails = new CourseDetails();
+		
+		courseDetails.setId(course.getId());
+		
+		courseDetails.setName(course.getName());
+		
+		courseDetails.setDescription(course.getDescription());
+		return courseDetails;
+	}
+
+	@PayloadRoot(namespace="http://vamsi.com/courses", 
+			localPart="GetAllCourseDetailsRequest")
+	@ResponsePayload
+	public GetAllCourseDetailsResponse processCourseDetailsRequest
+		(@RequestPayload GetAllCourseDetailsRequest request) {
+		
+		List<Course> courses = service.findAll();
+		
+		return mapAllCourseDetails(courses);
+	}
 }
--- src/main/java/com/vamsi/soap/webservices/soap_course_management/soap/WebServiceConfig.java
+++ src/main/java/com/vamsi/soap/webservices/soap_course_management/soap/WebServiceConfig.java
@@ -1,5 +1,56 @@
 package com.vamsi.soap.webservices.soap_course_management.soap;
 
-public class WebServiceConfig {
+import org.springframework.context.ApplicationContext;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.ws.config.annotation.EnableWs;
+import org.springframework.ws.transport.http.MessageDispatcherServlet;
+import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
+import org.springframework.xml.xsd.SimpleXsdSchema;
+import org.springframework.xml.xsd.XsdSchema;
 
+
+//Enable Spring Web Services
+@EnableWs
+//Spring Configuration
+@Configuration
+public class WebServiceConfig {
+	//MessageDispatcherServlet
+	// ApplicationContext
+	//url -> /ws/*
+	
+	@Bean
+	public ServletRegistrationBean messageDispatcherServlet(ApplicationContext context) {
+		MessageDispatcherServlet messageDispatcherServlet = 
+				new MessageDispatcherServlet();
+		messageDispatcherServlet.setApplicationContext(context);
+		messageDispatcherServlet.setTransformWsdlLocations(true);
+		return new ServletRegistrationBean(messageDispatcherServlet, "/ws/*");
+	}
+	
+	//  /ws/courses.wsdl
+	 // course-details.xsd
+	
+	@Bean(name="courses")
+	public DefaultWsdl11Definition defaultWsdl11Definition
+			(XsdSchema courseSchema) {
+		DefaultWsdl11Definition definition = new DefaultWsdl11Definition();
+		//PortType - CoursePort
+		definition.setPortTypeName("CoursePort");
+	    //NameSpace - http;//vamsi.com/courses
+		definition.setTargetNamespace("http://vamsi.com/courses");
+		// /ws
+		definition.setLocationUri("/ws");
+		//schema
+		definition.setSchema(courseSchema);
+		return definition;
+	}
+	
+	
+	@Bean
+	public XsdSchema coursesSchema() {
+		return new SimpleXsdSchema(new ClassPathResource("course-details.xsd"));
+	}
 }
